e6a7879c29755fb390f5cb14e6ffc6ffdadf0368
mazheng <zheng.ma@centurygame.com>
2025-09-15 20:29:02 +0800

    addexample

---
+++ examples/zinx_closecallback/router/ping_router.go
@@ -0,0 +1,33 @@
+package router
+
+import (
+	"fmt"
+
+	"github.com/aceld/zinx/ziface"
+	"github.com/aceld/zinx/znet"
+)
+
+// PingRouter handles ping messages
+// PingRouter 处理ping消息
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+// PreHandle processes the request before the main handler
+// PreHandle 在主处理器之前处理请求
+func (r *PingRouter) PreHandle(req ziface.IRequest) {
+}
+
+// Handle processes the main ping message and sends pong response
+// Handle 处理主要的ping消息并发送pong响应
+func (r *PingRouter) Handle(req ziface.IRequest) {
+	// Reply to client / 回复客户端
+	if err := req.GetConnection().SendMsg(0, []byte("Pong")); err != nil {
+		fmt.Println("SendMsg error:", err)
+	}
+}
+
+// PostHandle processes the request after the main handler
+// PostHandle 在主处理器之后处理请求
+func (r *PingRouter) PostHandle(req ziface.IRequest) {
+}
---
+++ examples/zinx_closecallback/server/server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aceld/zinx/examples/zinx_closecallback/router"
+	"github.com/aceld/zinx/ziface"
+	"github.com/aceld/zinx/znet"
+)
+
+// DoConnectionBegin is the callback function when connection starts
+// DoConnectionBegin 是连接开始时的回调函数
+func DoConnectionBegin(conn ziface.IConnection) {
+	fmt.Println("Server connection started")
+
+	// Set connection properties / 设置连接属性
+	conn.SetProperty("StartTime", time.Now())
+
+	// Add close callback function - cleanup resources / 添加关闭回调函数 - 清理资源
+	conn.AddCloseCallback("cleanup", "resources", func() {
+		fmt.Printf("Cleanup resources for connection %d\n", conn.GetConnID())
+	})
+
+	// Add close callback function - record statistics / 添加关闭回调函数 - 记录统计信息
+	conn.AddCloseCallback("stats", "record", func() {
+		if startTime, err := conn.GetProperty("StartTime"); err == nil {
+			duration := time.Since(startTime.(time.Time))
+			fmt.Printf("Connection %d duration: %v\n", conn.GetConnID(), duration)
+		}
+	})
+
+	// Add close callback function - notify other components / 添加关闭回调函数 - 通知其他组件
+	conn.AddCloseCallback("notification", "broadcast", func() {
+		fmt.Printf("Notify other components: connection %d disconnected\n", conn.GetConnID())
+	})
+}
+
+// DoConnectionLost is the callback function when connection is lost
+// DoConnectionLost 是连接断开时的回调函数
+func DoConnectionLost(conn ziface.IConnection) {
+	fmt.Println("Server connection lost")
+}
+
+func main() {
+	// Create server / 创建服务器
+	s := znet.NewServer()
+
+	// Set connection start and stop callbacks / 设置连接开始和断开的回调
+	s.SetOnConnStart(DoConnectionBegin)
+	s.SetOnConnStop(DoConnectionLost)
+
+	// Add router / 添加路由
+	s.AddRouter(1, &router.PingRouter{})
+
+	// Start server / 启动服务器
+	fmt.Println("Server starting")
+	s.Serve()
+}
